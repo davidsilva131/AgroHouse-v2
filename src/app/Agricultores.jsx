1be4d7e0667ae64582de267e73e09ae211e1ee67
David Silva <davidsilvac131@gmail.com>
2023-05-20 18:22:05 -0500

    Add components to principal page

---
+++ src/app/Agricultores.jsx
@@ -0,0 +1,9 @@
+const Agricultores = () => {
+  return (
+    <section className='w-full h-20 py-20 flex justify-center items-center border-b border-black border-opacity-30 '>
+      <h2>Agricultores</h2>
+    </section>
+  )
+}
+
+export default Agricultores
---
+++ src/app/LatestProducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LatestProducts = () => {
+  return (
+    <section className='w-full h-20 py-20 flex justify-center items-center border-b border-black border-opacity-30 '>
+      <h2>Lates Products</h2>
+    </section>
+  )
+}
+
+export default LatestProducts
