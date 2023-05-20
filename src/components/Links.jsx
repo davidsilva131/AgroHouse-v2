3fb6d1e77e90ed9a04bcce5bda22687ceb60eb88
David Silva <davidsilvac131@gmail.com>
2023-05-20 14:25:51 -0500

    Update to use pages links of constants file

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,26 +1,12 @@
 import Link from "next/link"
-
-const PAGES = [
-  {
-    text: 'Inicio',
-    url: '/'
-  },
-  {
-    text: 'Eventos',
-    url: '/eventos'
-  },
-  {
-    text: 'Productos',
-    url: '/productos'
-  }
-]
+import { PAGES_LINKS } from "./constants"
 
 const Links = () => {
   return (
     <nav className='hidden md:flex justify-center items-center pl-8'>
       <ul className='flex gap-7'>
         {
-          PAGES.map(page => (
+          PAGES_LINKS.map(page => (
             <li key={page.text}>
               <Link href={page.url} className='text-white uppercase transition-all ease-in-out duration-500 hover:text-green-400'>
                 {page.text}
