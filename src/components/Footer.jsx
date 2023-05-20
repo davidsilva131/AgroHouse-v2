b9b8e86a7d4ec95490e1bfe97a763f35c5eae36e
David Silva <davidsilvac131@gmail.com>
2023-05-20 18:13:11 -0500

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='w-full h-20 bg-green-700 flex justify-center items-center'>
+      <span className='text-white'>© AgroHouse | 2023</span>
+    </footer>
+  )
+}
+
+export default Footer
