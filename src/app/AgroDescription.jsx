99b83f065be3276ff93d1cb1828a6fb152c498a9
David Silva <davidsilvac131@gmail.com>
2023-05-20 18:22:36 -0500

    Update styles

--- src/app/AgroDescription.jsx
+++ src/app/AgroDescription.jsx
@@ -2,7 +2,7 @@ import { AGRO_DESCRIPTION } from "@/components/constants"
 import Image from "next/image"
 const AgroDescription = () => {
   return (
-    <section className='w-full flex flex-wrap mt-20 justify-center gap-4 animate-fade-right'>
+    <section className='w-full flex flex-wrap py-20 border-b border-black border-opacity-30 justify-center gap-4 animate-fade-right'>
       {
         AGRO_DESCRIPTION.map(desc => (
           <article className='w-80 flex flex-col items-center border border-green-700 border-opacity-25 shadow-lg rounded gap-3 px-4 py-3' key={desc.title}>
