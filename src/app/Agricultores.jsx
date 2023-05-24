c4a4d019024173f23dd4bb358f5d51d887cf25f0
David Silva <davidsilvac131@gmail.com>
2023-05-24 13:18:15 -0500

    Formating

--- src/app/Agricultores.jsx
+++ src/app/Agricultores.jsx
@@ -1,10 +1,15 @@
 'use client'
-import { useInView } from "react-intersection-observer"
+import { useInView } from 'react-intersection-observer'
 
-const Agricultores = ({farmers}) => {
+const Agricultores = ({ farmers }) => {
   const { ref: agricultoresRef, inView: agricultoresIsVisible } = useInView()
   return (
-    <section ref={agricultoresRef} className={`w-full h-20 py-20 flex justify-center items-center border-b border-black border-opacity-30 ${agricultoresIsVisible && 'animate-fade-right'}`}>
+    <section
+      ref={agricultoresRef}
+      className={`w-full h-20 py-20 flex justify-center items-center border-b border-black border-opacity-30 ${
+        agricultoresIsVisible && 'animate-fade-right'
+      }`}
+    >
       <h2>Agricultores</h2>
     </section>
   )
