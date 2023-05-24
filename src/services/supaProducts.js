ad77edb7d6e0a73f6b9801c91911428dd7302fc8
David Silva <davidsilvac131@gmail.com>
2023-05-24 13:14:34 -0500

    Add functions to get info from supabase

--- src/services/supaProducts.js
+++ src/services/supaProducts.js
@@ -1,10 +1,16 @@
 import { supabase } from '@/services/supabaseConfig'
 
-export async function getSupaProducts () {
-  let { data: products, error } = await
-    supabase
-      .from('products')
-      .select('*')
+export async function getSupaProducts() {
+  let { data: products, error } = await supabase.from('products').select('*')
 
   return products
+}
+
+export async function getSupaLatestProducts() {
+  let { data: latesProducts, error } = await supabase
+    .from('products')
+    .select('*')
+    .range(0, 3)
+
+  return latesProducts
 }
