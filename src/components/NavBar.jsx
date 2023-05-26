9d48ae3eb39d62d0dce79c9b253fffa44bc9f6e6
David Silva <davidsilvac131@gmail.com>
2023-05-26 09:56:13 -0500

    Update to have logout button (need to apply conditional rendering)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,23 @@
 'use client'
-import DialogLogIn from "./DialogLogIn"
-import DialogRegister from "./DialogRegister"
-import DropMenu from "./DropMenu"
-import LinkLogo from "./LinkLogo"
-import Links from "./Links"
-
+import { useState } from 'react'
+import DialogLogIn from './DialogLogIn'
+import DialogRegister from './DialogRegister'
+import DropMenu from './DropMenu'
+import LinkLogo from './LinkLogo'
+import Links from './Links'
+import { signOutUser } from '@/services/supaUser'
+import { useRouter } from 'next/navigation'
 const NavBar = () => {
+  const router = useRouter()
+  const [session, setSession] = useState(null)
+
+  const handleSignOut = async () => {
+    await signOutUser()
+    router.refresh()
+  }
+
   return (
-    <header className="w-full h-20 md:h-32 flex items-center px-4 bg-green-700">
+    <header className='w-full h-20 md:h-32 flex items-center px-4 bg-green-700'>
       <section className='hidden md:flex'>
         <LinkLogo />
       </section>
@@ -17,8 +27,16 @@ const NavBar = () => {
         </div>
         <Links />
         <div className='flex gap-4'>
-          <DialogRegister />
-          <DialogLogIn />
+          <button
+            onClick={handleSignOut}
+            type='button'
+            className='w-fit p-1 flex justify-center items-center bg-white rounded text-xs md:text-base md:p-2 hover:bg-opacity-80 hover:shadow-sm'
+          >
+            Cerrar Sesion
+          </button>
+
+          <DialogRegister setSession={setSession} />
+          <DialogLogIn setSession={setSession} />
         </div>
       </section>
     </header>
